56e929dc08b91a09438a4a57ed263c20677b1623
Mateusz Jablonski <mateusz.jablonski@codilime.com>
2018-05-22 15:57:34 +0200

    Add missing property

--- CIScripts/Test/Utils/CommonTestCode.ps1
+++ CIScripts/Test/Utils/CommonTestCode.ps1
@@ -14,6 +14,7 @@ class ContainerNetAdapterInformation : NetAdapterInformation {
     [string] $AdapterShortName;
     [string] $AdapterFullName;
     [string] $IPAddress;
+    [int] $MtuSize;
 }
 
 class VMNetAdapterInformation : NetAdapterMacAddresses {
